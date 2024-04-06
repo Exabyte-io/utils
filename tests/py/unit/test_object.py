c96474148001b672c63cea9d19728836f57a9608
Timur Bazhirov <timur@exabyte.io>
2024-04-05 19:11:30 -0700

    chore: fix tests

--- tests/py/unit/test_object.py
+++ tests/py/unit/test_object.py
@@ -1,9 +1,13 @@
+import copy
 from mat3ra.utils import object as utils
 
 REFERENCE_OBJECT_1 = {"key1": "value1", "key2": "value2"}
 REFERENCE_OBJECT_1_WITHOUT_KEY1 = {"key2": "value2"}
 REFERENCE_OBJECT_1_WITH_KEY3 = {"key1": "value1", "key2": "value2", "key3": "value3"}
-REFERENCE_OBJECT_2 = {"key2": {"nested_key1": "nested_value1"}}
+# Using 3 different objects to avoid clashes between tests
+REFERENCE_OBJECT_2_CLONE_SHALLOW = {"key2": {"nested_key1": "nested_value1"}}
+REFERENCE_OBJECT_2_CLONE_DEEP = {"key2": {"nested_key1": "nested_value1"}}
+REFERENCE_OBJECT_2_GET = {"key2": {"nested_key1": "nested_value1"}}
 
 
 def test_omit():
@@ -18,19 +22,19 @@ def test_set():
 
 
 def test_clone_shallow():
-    object_2_clone_shallow = utils.clone_shallow(REFERENCE_OBJECT_2)
-    REFERENCE_OBJECT_2["key2"]["nested_key1"] = "nested_value2"
+    object_2_clone_shallow = utils.clone_shallow(REFERENCE_OBJECT_2_CLONE_SHALLOW)
+    REFERENCE_OBJECT_2_CLONE_SHALLOW["key2"]["nested_key1"] = "nested_value2"
     assert object_2_clone_shallow["key2"]["nested_key1"] == "nested_value2"
 
 
 def test_clone_deep():
-    object_2_clone_deep = utils.clone_deep(REFERENCE_OBJECT_2)
-    REFERENCE_OBJECT_2["key2"]["nested_key1"] = "nested_value2"
+    object_2_clone_deep = utils.clone_deep(REFERENCE_OBJECT_2_CLONE_DEEP)
+    REFERENCE_OBJECT_2_CLONE_DEEP["key2"]["nested_key1"] = "nested_value2"
     assert object_2_clone_deep["key2"]["nested_key1"] == "nested_value1"
 
 
 def test_get():
-    nested_value1 = utils.get(REFERENCE_OBJECT_2, "key2/nested_key1")
+    nested_value1 = utils.get(REFERENCE_OBJECT_2_GET, "key2/nested_key1")
     assert nested_value1 == "nested_value1"
-    assert nested_value1 == utils.get(REFERENCE_OBJECT_2, "/key2/nested_key1")
-    assert nested_value1 == utils.get(REFERENCE_OBJECT_2, "key2.nested_key1", ".")
+    assert nested_value1 == utils.get(REFERENCE_OBJECT_2_GET, "/key2/nested_key1")
+    assert nested_value1 == utils.get(REFERENCE_OBJECT_2_GET, "key2.nested_key1", ".")

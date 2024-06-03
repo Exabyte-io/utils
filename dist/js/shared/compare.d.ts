4214f52a7c9f314453426cffef6bca4d7f0a6b29
Kostiantyn Dvornik <dvornik.konstantin@gmail.com>
2024-06-03 14:18:30 +0300

    chore: cleanup dist

--- dist/js/shared/compare.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function shallowDeepAlmostEqual(
-    expect?: object | boolean | number | null | string | Date,
-    actual?: object | boolean | number | null | string | Date,
-    path?: string,
-    threshold?: number,
-): boolean;

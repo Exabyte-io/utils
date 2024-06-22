2562512eca2e5de6ddec83725ed078d2deed82fd
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-06-21 17:16:06 -0700

    update: use paramters

--- src/py/mat3ra/utils/mixins.py
+++ src/py/mat3ra/utils/mixins.py
@@ -7,4 +7,4 @@ class RoundNumericValuesMixin(object):
 
     @classmethod
     def round_array_or_number(cls, array, decimal_places=__round_precision__):
-        return np.round(array, cls.__round_precision__).tolist()
+        return np.round(array, decimal_places).tolist()

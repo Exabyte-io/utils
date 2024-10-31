4ac6e1143640872f6799c250c08a0ebe3663b49d
Timur Bazhirov <timur@exabyte.io>
2024-10-30 20:58:13 -0700

    chore: fix lint

--- tests/py/unit/test_url.py
+++ tests/py/unit/test_url.py
@@ -2,5 +2,5 @@ from mat3ra.utils import url as utils
 
 
 def test_is_url():
-    assert utils.is_url("https://google.com") == True
-    assert utils.is_url("test_string") == False
+    assert utils.is_url("https://google.com") is True
+    assert utils.is_url("test_string") is False

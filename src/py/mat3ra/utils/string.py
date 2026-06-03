0582b104400fc2464aeebb126213acdec7d2fce9
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2026-06-03 23:50:33 +0800

    add comment removal for fortran lang

--- src/py/mat3ra/utils/string.py
+++ src/py/mat3ra/utils/string.py
@@ -60,7 +60,12 @@ def snake_to_camel(snake_case_str: str) -> str:
 
 
 def remove_comments_from_source_code(text: str, language: str = "shell") -> str:
-    """Removes lines starting with # (except shebang)."""
+    """Removes comments from source code based on the language."""
+    if language == "fortran":
+        # Removes inline and full-line comments starting with ! or #
+        return re.sub(r"[!#].*$", "", text, flags=re.MULTILINE)
+
+    # Default (shell): Removes lines starting with # (except shebang)
     return re.sub(r"^(\s+)?#(?!!).*$", "", text, flags=re.MULTILINE)
 
 
